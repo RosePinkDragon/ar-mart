7aa1a76fc89308551844361aef9c26ad98b5f845
RosePinkDragon <59683036+RosePinkDragon@users.noreply.github.com>
2021-08-10 18:45:14 +0530

    A working initial repo
    Hey gang this is the initial repo please add in the stuff as you go

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "About" });
+});
+
+app.get("/home", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/create", (req, res) => {
+  res.render("create", { title: "Create A Blog" });
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "Lost Page" });
+});
